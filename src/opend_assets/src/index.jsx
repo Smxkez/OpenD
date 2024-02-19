eb260cd0d6a27048bf19eab27b1c33dc98665ba7
Smxkez <53111840+Smxkez@users.noreply.github.com>
2024-02-19 22:36:21 +0200

    Update index.jsx

--- src/opend_assets/src/index.jsx
+++ src/opend_assets/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Principal } from "@dfinity/principal";
 
-const CURRENT_USER_ID = Principal.fromText("2vxsx-fae");
+const CURRENT_USER_ID = Principal.fromText("sensitive");
 export default CURRENT_USER_ID;
 
 const init = async () => {
